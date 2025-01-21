580940aebf1f261a781acb25ed926c98262a6eba
Yamelnyk <yachkana@ukr.net>
2025-01-22 01:30:59 +0200

    header

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+const mobileMenuBtn = document.querySelector(".mobile-menu-open-btn");
+const mobileMenuCloseBtn = document.querySelector(".mobile-menu-close-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenuBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("is-open");
+  document.body.style.overflow = "hidden";
+});
+
+mobileMenuCloseBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("is-open");
+  document.body.style.overflow = "auto";
+});
+
+console.log("Yana");
