955d727729ce4c0d85eb796b96fff0bb56bc7a76
Yamelnyk <yachkana@ukr.net>
2025-01-29 12:17:25 +0200

    touchstart add

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import './style.css';
 
-const mobileMenuBtn = document.querySelector(".mobile-menu-open-btn");
-const mobileMenuCloseBtn = document.querySelector(".mobile-menu-close-btn");
-const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuBtn = document.querySelector('.mobile-menu-open-btn');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-mobileMenuBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("is-open");
-  document.body.style.overflow = "hidden";
+mobileMenuBtn.addEventListener('touchstart', () => {
+  mobileMenu.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
 });
 
-mobileMenuCloseBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("is-open");
-  document.body.style.overflow = "auto";
+mobileMenuCloseBtn.addEventListener('touchstart', () => {
+  mobileMenu.classList.remove('is-open');
+  document.body.style.overflow = 'auto';
 });
