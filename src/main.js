598895c0069b8f69f79fd1d2771816cbe8307f01
Yamelnyk <yachkana@ukr.net>
2025-01-29 12:46:25 +0200

    click

--- src/main.js
+++ src/main.js
@@ -4,19 +4,12 @@ const mobileMenuBtn = document.querySelector('.mobile-menu-open-btn');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-function openMenu() {
+mobileMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.add('is-open');
   document.body.style.overflow = 'hidden';
-}
+});
 
-function closeMenu() {
+mobileMenuCloseBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('is-open');
   document.body.style.overflow = 'auto';
-}
-
-// Додаємо підтримку як `touchstart`, так і `click`
-mobileMenuBtn.addEventListener('touchstart', openMenu);
-mobileMenuBtn.addEventListener('click', openMenu);
-
-mobileMenuCloseBtn.addEventListener('touchstart', closeMenu);
-mobileMenuCloseBtn.addEventListener('click', closeMenu);
+});
