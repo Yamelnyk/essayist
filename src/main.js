4234309e4a40b4a204edc95fdc8c39810ead5bd1
Yamelnyk <yachkana@ukr.net>
2025-02-06 02:51:08 +0200

    Reviews section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* -------------MOBAIL_MENU----------- */
+
 const mobileMenuBtn = document.querySelector('.mobile-menu-open-btn');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -10,4 +12,23 @@ mobileMenuBtn.addEventListener('click', () => {
 mobileMenuCloseBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('is-open');
   document.body.style.overflow = 'auto';
+});
+
+/* --------- Reviews ----------- */
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('myBtn')) {
+    const reviewBlock = event.target.closest('.reviews-block-text');
+    const dots = reviewBlock.querySelector('.dots');
+    const moreText = reviewBlock.querySelector('.more');
+
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      event.target.textContent = 'Read more';
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      event.target.textContent = 'Collapse';
+      moreText.style.display = 'inline';
+    }
+  }
 });
