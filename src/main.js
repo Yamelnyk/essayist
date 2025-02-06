7be0ca44abdae27743983afddefe2c5c9df317fe
Yamelnyk <yachkana@ukr.net>
2025-02-06 14:08:18 +0200

    review modal

--- src/main.js
+++ src/main.js
@@ -15,20 +15,61 @@ mobileMenuCloseBtn.addEventListener('click', () => {
 });
 
 /* --------- Reviews ----------- */
+// document.addEventListener('click', event => {
+//   if (event.target.classList.contains('myBtn')) {
+//     const reviewBlock = event.target.closest('.reviews-block-text');
+//     const dots = reviewBlock.querySelector('.dots');
+//     const moreText = reviewBlock.querySelector('.more');
+
+//     if (dots.style.display === 'none') {
+//       dots.style.display = 'inline';
+//       event.target.textContent = 'Read more';
+//       moreText.style.display = 'none';
+//     } else {
+//       dots.style.display = 'none';
+//       event.target.textContent = 'Collapse';
+//       moreText.style.display = 'inline';
+//     }
+//   }
+// });
+
 document.addEventListener('click', event => {
   if (event.target.classList.contains('myBtn')) {
     const reviewBlock = event.target.closest('.reviews-block-text');
-    const dots = reviewBlock.querySelector('.dots');
-    const moreText = reviewBlock.querySelector('.more');
 
-    if (dots.style.display === 'none') {
-      dots.style.display = 'inline';
-      event.target.textContent = 'Read more';
-      moreText.style.display = 'none';
+    const reviewTitle = reviewBlock.querySelector(
+      '.reviews-block-title'
+    ).textContent;
+    const fullReviewText = reviewBlock.querySelector('.text').textContent;
+
+    if (window.innerWidth >= 768) {
+      const modal = document.getElementById('reviewModal');
+
+      document.querySelector('.modal-title').textContent = reviewTitle;
+      document.querySelector('.modal-text').textContent =
+        fullReviewText.replace('...', '');
+      modal.style.display = 'flex';
+
+      document.querySelector('.close').onclick = () =>
+        (modal.style.display = 'none');
+      window.onclick = e => {
+        if (e.target === modal) {
+          modal.style.display = 'none';
+        }
+      };
     } else {
-      dots.style.display = 'none';
-      event.target.textContent = 'Collapse';
-      moreText.style.display = 'inline';
+      const dots = reviewBlock.querySelector('.dots');
+      const moreText = reviewBlock.querySelector('.more');
+
+      if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        event.target.textContent = 'Read more';
+        moreText.style.display = 'none';
+      } else {
+        dots.style.display = 'none';
+        event.target.textContent = 'Collapse';
+        moreText.style.display = 'inline';
+      }
     }
   }
 });
